2c2630ad05bf04c88f2278accd9736c525027334
inpiniti <inpiniti@gmail.com>
2024-08-01 15:30:04 +0900

    히스토리 저장

---
+++ server/utils/useHistory.ts
@@ -0,0 +1,54 @@
+import { sql } from "drizzle-orm";
+
+export const useHistory = () => {
+  const seoulInsert = async (data: any) => {
+    try {
+      const firstRowParamCount = Object.keys(data[0]).length; // 첫 번째 행의 파라미터 수를 계산합니다.
+      const chunkSize = Math.floor(65534 / firstRowParamCount); // 83은 각 행의 파라미터 수입니다.
+      const dataChunks = splitData(data, chunkSize);
+
+      // 분할된 데이터 삽입
+      for (const chunk of dataChunks) {
+        await useGalaxy().insert(pgTableSeoul).values(chunk);
+      }
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const kosdaqInsert = async (data: any) => {
+    try {
+      const firstRowParamCount = Object.keys(data[0]).length; // 첫 번째 행의 파라미터 수를 계산합니다.
+      const chunkSize = Math.floor(65534 / firstRowParamCount); // 83은 각 행의 파라미터 수입니다.
+      const dataChunks = splitData(data, chunkSize);
+
+      // 분할된 데이터 삽입
+      for (const chunk of dataChunks) {
+        await useGalaxy().insert(pgTableKosdaq).values(chunk);
+      }
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const nasdaqInsert = async (data: any) => {
+    try {
+      const firstRowParamCount = Object.keys(data[0]).length; // 첫 번째 행의 파라미터 수를 계산합니다.
+      const chunkSize = Math.floor(65534 / firstRowParamCount); // 83은 각 행의 파라미터 수입니다.
+      const dataChunks = splitData(data, chunkSize);
+
+      // 분할된 데이터 삽입
+      for (const chunk of dataChunks) {
+        await useGalaxy().insert(pgTableNasdaq).values(chunk);
+      }
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+};
