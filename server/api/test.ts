51e5ca4b45ee559f7f217bcda28602e94695e041
youngkyunjung <younginpiniti@gmail.com>
2024-08-07 22:21:03 +0900

    인베스팅 크롤링 코드 수정

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,14 +1,12 @@
-export default defineEventHandler((event) => {
-  console.log(1);
-  console.log(2);
-  console.log(3);
-  console.log(4);
-  console.log(5);
-  console.log(6);
-  console.log(7);
-  console.log(8);
-  console.log(9);
-  console.log(10);
+import { getLoopData } from "../api/investing/code";
+
+export default defineEventHandler(async (event) => {
+  const data = await getLoopData({
+    country: "KR",
+    market: "Seoul",
+  });
+  console.log(data.length);
+
   return {
     hello: "world",
   };
