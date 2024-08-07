59c9296e59076b6f4495962c4066b822dea82cc7
inpiniti <inpiniti@gmail.com>
2024-08-08 07:54:12 +0900

    에러 코드 추가 016까지 추가됨

--- server/api/investing/code.ts
+++ server/api/investing/code.ts
@@ -17,24 +17,29 @@ export const getLoopData = async ({
   country: string;
   market: string;
 }) => {
-  let skip = 0;
+  try {
+    let skip = 0;
 
-  const response: any = await getData({ skip, country, market });
-  const result = response;
+    const response: any = await getData({ skip, country, market });
+    const result = response;
 
-  // skip 을 100 씩 올려가면서, 다음페이지도 조회하도록 코드를 작성해줘
-  while (result.page.hasNextPage) {
-    skip += 100;
-    const nextResponse: any = await getData({ skip, country, market });
+    // skip 을 100 씩 올려가면서, 다음페이지도 조회하도록 코드를 작성해줘
+    while (result.page.hasNextPage) {
+      skip += 100;
+      const nextResponse: any = await getData({ skip, country, market });
 
-    const nextResult = nextResponse;
-    result.rows = result.rows.concat(nextResult.rows);
-    result.page = nextResult.page;
+      const nextResult = nextResponse;
+      result.rows = result.rows.concat(nextResult.rows);
+      result.page = nextResult.page;
+    }
+
+    console.log("rowlength", result.rows.length);
+
+    return ticker({ result, country, market });
+  } catch (error) {
+    console.error("error 016", error);
+    throw error;
   }
-
-  console.log("rowlength", result.rows.length);
-
-  return ticker({ result, country, market });
 };
 
 // ticker 추출
@@ -71,44 +76,45 @@ const getData = ({
   country: string;
   market: string;
 }) => {
-  console.log("skip", skip);
+  try {
+    console.log("skip", skip);
 
-  const body = JSON.stringify({
-    query: {
-      filters: [],
-      sort: {
-        metric: "marketcap_adj_latest",
-        direction: "DESC",
+    const body = JSON.stringify({
+      query: {
+        filters: [],
+        sort: {
+          metric: "marketcap_adj_latest",
+          direction: "DESC",
+        },
+        prefilters: {
+          market: country,
+          primaryOnly: true,
+          exchange: market,
+        },
       },
-      prefilters: {
-        market: country,
-        primaryOnly: true,
-        exchange: market,
+      metrics: [
+        "investing_exchange",
+        "investing_sector",
+        "investing_industry",
+        "marketcap_adj_latest",
+        "pe_ltm_latest",
+        "peg_ltm",
+        "asset_price_latest",
+        "asset_price_latest_change_pct",
+        "fair_value",
+        "fair_value_upside",
+        "fair_value_label",
+        "analyst_target",
+        "analyst_target_upside",
+        "fin_health_overall_label",
+      ],
+      page: {
+        skip,
+        limit: 100,
       },
-    },
-    metrics: [
-      "investing_exchange",
-      "investing_sector",
-      "investing_industry",
-      "marketcap_adj_latest",
-      "pe_ltm_latest",
-      "peg_ltm",
-      "asset_price_latest",
-      "asset_price_latest_change_pct",
-      "fair_value",
-      "fair_value_upside",
-      "fair_value_label",
-      "analyst_target",
-      "analyst_target_upside",
-      "fin_health_overall_label",
-    ],
-    page: {
-      skip,
-      limit: 100,
-    },
-  });
+    });
 
-  const curlCommand = `
+    const curlCommand = `
   curl -s 'https://www.investing.com/pro/_/screener-v2/query' \
     -H 'accept: */*' \
     -H 'accept-language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7' \
@@ -128,24 +134,28 @@ const getData = ({
     --data-raw '${body}'
   `;
 
-  return new Promise((resolve, reject) => {
-    exec(curlCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${error.message}`);
-        reject(error);
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        reject(stderr);
-        return;
-      }
-      try {
-        const jsonResponse = JSON.parse(stdout);
-        resolve(jsonResponse);
-      } catch (parseError) {
-        reject(parseError);
-      }
+    return new Promise((resolve, reject) => {
+      exec(curlCommand, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          reject(error);
+          return;
+        }
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          reject(stderr);
+          return;
+        }
+        try {
+          const jsonResponse = JSON.parse(stdout);
+          resolve(jsonResponse);
+        } catch (parseError) {
+          reject(parseError);
+        }
+      });
     });
-  });
+  } catch (error) {
+    console.error("error 015", error);
+    throw error;
+  }
 };
